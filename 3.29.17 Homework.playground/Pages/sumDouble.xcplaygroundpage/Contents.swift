762ac467e6a3ddf641205bdb9e98c63345159f7d
Crystal Jade Allen-Washington <crystaljade@Macbooks-MacBook-Air.local>
2017-04-05 14:06:11 -0400

    Second commit - updated sumDouble activity with accurate code.

--- 3.29.17 Homework.playground/Pages/sumDouble.xcplaygroundpage/Contents.swift
+++ 3.29.17 Homework.playground/Pages/sumDouble.xcplaygroundpage/Contents.swift
@@ -1,17 +1,15 @@
 // Given two int values, return their sum. Unless the two values are the same, then return double their sum.
 
-// Desired steps: 
-//      (1) Establish equation for summary - WHERE I'M GETTING STUCK. 
 
-// let intSum = first(Int) + second(Int)
-
-// func sumDouble(first: Int, second: Int) -> intSum {
-//        if first(Int) = second(Int) {
- //           return (intSum * 2) {
+func sumDouble(valueOne: Int, valueTwo: Int) -> Int {
+    if valueOne == valueTwo {
+        return (valueOne + valueTwo) * 2
+    } else {
+        return (valueOne + valueTwo)
     }
 }
 
 
-sumDouble(first: 1, second: 2) == 3
-sumDouble(first: 3, second: 2) == 5
-sumDouble(first: 2, second: 2) == 8
+sumDouble(valueOne: 1, valueTwo: 2) == 3
+sumDouble(valueOne: 3, valueTwo: 2) == 5
+sumDouble(valueOne: 2, valueTwo: 2)
